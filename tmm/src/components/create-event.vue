5c9413876ecad8697428b976e4b3703f86a3798b
Vitaliy B <shaggy@Vitaliys-MacBook-Pro.local>
2017-11-01 23:28:05 +0200

    daterengepicker added

--- tmm/src/components/create-event.vue
+++ tmm/src/components/create-event.vue
@@ -9,10 +9,16 @@
                     ></b-form-input>
                 </b-form-group>
                 <b-form-group label="Date:" label-for="eventDate">
-                    <b-form-input id="eventDate" type="text" required
-                                  placeholder="Enter event date"
-                                   v-model="newEvent.date"
-                    ></b-form-input>
+                    <!--<b-form-input id="eventDate" type="text" required-->
+                                  <!--placeholder="Enter event date"-->
+                                   <!--v-model="newEvent.date"-->
+                    <!--&gt;</b-form-input>-->
+                    <app-datepicker
+                            id="eventDate"
+                            :i18n="ptBr"
+                            v-model="newEvent.date"
+                            v-on:checkOutChanged="getDate">
+                    </app-datepicker>
                 </b-form-group>
                 <div class="float-right">
                     <b-button type="submit" variant="primary">Submit</b-button>
@@ -27,6 +33,7 @@
 
 <script>
     import {db} from '../firebase';
+    import Datepicker from 'vue-hotel-datepicker';
 
     export default {
         data() {
@@ -35,6 +42,14 @@
                     name: '',
                     date: '',
                 },
+                ptBr: {
+                    night: 'Night',
+                    nights: 'Nights',
+                    'day-names': ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                    'check-in': 'Start Date',
+                    'check-out': 'End Date',
+                    'month-names': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                }
             }
         },
         firebase: {
@@ -42,18 +57,29 @@
                 source: db.ref('events')
             }
         },
+        components: {
+            'app-datepicker': Datepicker
+        },
         methods: {
             onSubmit(evt) {
                 evt.preventDefault();
-//                alert(JSON.stringify(this.form));
-//                var json = JSON.stringify(this.form);
                 this.hideModal();
                 this.$refs.alert.showAlert('success', 'Event created!');
 
                 this.$firebaseRefs.events.push({name: this.newEvent.name, date: this.newEvent.date});
             },
-            notification() {
+            getDate() {
+                let dates = [];
+                dates = document.getElementsByClassName('datepicker__input');
 
+                var d = setTimeout(function () {
+                    let sd, ed;
+                    sd = dates[0].value.toString();
+                    ed = dates[1].value.toString();
+                    return sd + ' - ' + ed;
+                },100);
+                console.log(d);
+                this.newEvent.date = '' + d;
             },
             hideModal() {
                 this.$refs.modalCreateEvent.hide();
